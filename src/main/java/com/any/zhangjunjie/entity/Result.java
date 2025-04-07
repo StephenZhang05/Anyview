aff06756497ea3e44d8a997871cd7171747e5423
StephenZhang05 <948702570@qq.com>
2025-04-07 10:39:48 +0800

    响应体泛型

--- src/main/java/com/any/zhangjunjie/entity/Result.java
+++ src/main/java/com/any/zhangjunjie/entity/Result.java
@@ -1,8 +1,30 @@
 package com.any.zhangjunjie.entity;
 
+import java.util.List;
+
 /**
  * @author zhangjunjie
  * 结果泛型
  */
 public class Result {
+    private Boolean success;
+    private String errorMsg;
+    private Object data;
+    private Long total;
+
+    public Result(boolean b, Object o, Object o1, Object o2) {
+    }
+
+    public static Result ok(){
+        return new Result(true, null, null, null);
+    }
+    public static Result ok(Object data){
+        return new Result(true, null, data, null);
+    }
+    public static Result ok(List<?> data, Long total){
+        return new Result(true, null, data, total);
+    }
+    public static Result fail(String errorMsg){
+        return new Result(false, errorMsg, null, null);
+    }
 }
